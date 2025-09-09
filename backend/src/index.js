be193489077e5cc11bbb11ecbd0ea3b928677f64
Sanchit Kumar <hcpssanchit@gmail.com>
2025-09-09 21:32:58 +0530

    deployment changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js" 
 import cors from "cors"
 
+import path from "path";
+
 import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser"
 import { app,server} from "./lib/socket.js"
@@ -21,8 +23,17 @@ app.use(cors({
 }));
 
 const PORT=process.env.PORT;
+const __dirname=path.resolve();
 app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
+app.use("/api/messages",messageRoutes);
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
 
 server.listen(PORT,()=>{
     console.log("server is listening on PORT:"+PORT);
