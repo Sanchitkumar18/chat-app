6860cc2a10fb425eda19af487ba50ff85c8a0e44
Sanchit Kumar <hcpssanchit@gmail.com>
2025-09-09 22:32:12 +0530

    node_modules issues fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" })); // Also update t
 
 app.use(cookieParser())
 app.use(cors({
-    origin:process.env.CLIENT_URL || "http://localhost:5173",
+    origin:"http://localhost:5173",
     credentials:true
 }));
 
