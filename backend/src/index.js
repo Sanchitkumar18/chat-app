8b2671d676e762aa419488f9dc2624e7d99d855a
Sanchit Kumar <hcpssanchit@gmail.com>
2025-09-09 23:02:35 +0530

    fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "../routes/auth.route.js";
-import messageRoutes from "../routes/message.route.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
